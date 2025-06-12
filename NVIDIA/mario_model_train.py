d44ce924d4b257814305508ffe6eb092b7bae913
Xinzhou Lu <Xinzhou.Lu@hhu.de>
2025-06-12 15:37:48 +0200

    initialization

--- NVIDIA/mario_model_train.py
+++ NVIDIA/mario_model_train.py
@@ -7,5 +7,5 @@ env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 model = PPO('CnnPolicy', env, verbose=1, tensorboard_log='mario_train_logs')
-model.learn(total_timesteps=100)
+model.learn(total_timesteps=1000)
 model.save('mario_model')
